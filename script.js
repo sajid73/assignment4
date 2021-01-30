1f7b3813067f1b489a569fc8d970c8661920adb9
SM7 <smsajid7300@gmail.com>
2021-01-30 15:03:02 +0600

    some improvement on messege showing

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// This function for plus / minus button to increase or decrease ticket quantity
 function quantityChanger(classType, isIncrease){
     var type =  document.getElementById(classType).value
     if(isIncrease == true){
@@ -12,6 +13,7 @@ function quantityChanger(classType, isIncrease){
     totalUpdate();
 }
 
+// To updating subtotal and total price with tax
 function totalUpdate(){
     var economyClass = document.getElementById('economyclass-quantity').value
     var firstClass = document.getElementById('firstclass-quantity').value
@@ -21,15 +23,30 @@ function totalUpdate(){
     document.getElementById('tax').innerText = tax
     var total = subtotal + tax
     document.getElementById('total').innerText = total
+    return total
 }
 
+// To handle booking confirmation and error
 function confirmationArea(){
     document.getElementById('mainarea').style.display = "none"
+    var totalPrice = totalUpdate();
     const totalCheck = document.getElementById("total").innerText;
     if(totalCheck == 0){
         document.getElementById('noticket-box').style.display = "block"
     }
     else{
         document.getElementById('confirm-box').style.display = "block"
+        const economyClass = document.getElementById('economyclass-quantity').value
+        const firstClass = document.getElementById('firstclass-quantity').value
+        const confirmBox = document.getElementById('confrim-messege')
+        if(firstClass > 0 && economyClass > 0){
+            confirmBox.innerText = firstClass + " first class & " + economyClass + " economy class ticket\nTotal : $" + totalPrice
+        }
+        else if(firstClass > 0){
+            confirmBox.innerText = firstClass + " first class ticket\nTotal : $" + totalPrice
+        }
+        else{
+            confirmBox.innerText = economyClass + " economy class ticket\nTotal : $" + totalPrice
+        }
     }
 }
