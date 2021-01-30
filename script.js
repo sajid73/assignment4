fd87c0b11d15a0d9ee336c5121ac8474ae617667
SM7 <smsajid7300@gmail.com>
2021-01-30 15:48:26 +0600

    final touch

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // This function for plus / minus button to increase or decrease ticket quantity
-function quantityChanger(classType, isIncrease){
-    var type =  document.getElementById(classType).value
-    if(isIncrease == true){
+function quantityChanger(classType, isIncrease) {
+    var type = document.getElementById(classType).value
+    if (isIncrease == true) {
         document.getElementById(classType).value = ++type
     }
-    else{
-        if(type == 0){
+    else {
+        if (type == 0) {
             return
         }
         document.getElementById(classType).value = --type
@@ -14,10 +14,10 @@ function quantityChanger(classType, isIncrease){
 }
 
 // To updating subtotal and total price with tax
-function totalUpdate(){
-    var economyClass = document.getElementById('economyclass-quantity').value
-    var firstClass = document.getElementById('firstclass-quantity').value
-    var subtotal = firstClass*150 + economyClass*100;
+function totalUpdate() {
+    var economyClassQuantity = document.getElementById('economyclass-quantity').value
+    var firstClassQuantity = document.getElementById('firstclass-quantity').value
+    var subtotal = firstClassQuantity * 150 + economyClassQuantity * 100;
     document.getElementById('subtotal').innerText = subtotal
     var tax = subtotal * 0.1
     document.getElementById('tax').innerText = tax
@@ -27,26 +27,28 @@ function totalUpdate(){
 }
 
 // To handle booking confirmation and error
-function confirmationArea(){
+function confirmationArea() {
     document.getElementById('mainarea').style.display = "none"
     var totalPrice = totalUpdate();
     const totalCheck = document.getElementById("total").innerText;
-    if(totalCheck == 0){
+    if (totalCheck == 0) {
         document.getElementById('noticket-box').style.display = "block"
     }
-    else{
+    else {
         document.getElementById('confirm-box').style.display = "block"
         const economyClass = document.getElementById('economyclass-quantity').value
         const firstClass = document.getElementById('firstclass-quantity').value
         const confirmBox = document.getElementById('confrim-messege')
-        if(firstClass > 0 && economyClass > 0){
-            confirmBox.innerText = firstClass + " first class & " + economyClass + " economy class ticket\nTotal : $" + totalPrice
+
+        // showing ticket quantity and price details
+        if (firstClass > 0 && economyClass > 0) {
+            confirmBox.innerText = firstClass + " first class & " + economyClass + " economy class ticket\n\nTotal : $" + totalPrice
         }
-        else if(firstClass > 0){
-            confirmBox.innerText = firstClass + " first class ticket\nTotal : $" + totalPrice
+        else if (firstClass > 0) {
+            confirmBox.innerText = firstClass + " first class ticket\n\nTotal : $" + totalPrice
         }
-        else{
-            confirmBox.innerText = economyClass + " economy class ticket\nTotal : $" + totalPrice
+        else {
+            confirmBox.innerText = economyClass + " economy class ticket\n\nTotal : $" + totalPrice
         }
     }
 }
